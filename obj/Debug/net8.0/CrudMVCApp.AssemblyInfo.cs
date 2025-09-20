028463b8c98c626b5b16d210cc349a325a714682
DFacundoBarrios <faqbarrios23@gmail.com>
2025-09-20 17:58:24 -0300

    Se agrego el proyecto

--- obj/Debug/net8.0/CrudMVCApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/CrudMVCApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CrudMVCApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+485a186523977de62d2331ecdb3e3c3b1cb4573e")]
 [assembly: System.Reflection.AssemblyProductAttribute("CrudMVCApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CrudMVCApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
